4dd5247360f55214ccb522b44cd4d0b019c802bb
Alpha-fury-21 <alphafury21@gmail.com>
2023-07-20 17:58:38 +0530

    added sdk file

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+    const firebaseConfig = {
+    apiKey: "AIzaSyCAv0SWmMCAJfEubeoVWl1hQk73MzM_czo",
+    authDomain: "voting-3fdb6.firebaseapp.com",
+    databaseURL: "https://voting-3fdb6-default-rtdb.firebaseio.com",
+    projectId: "voting-3fdb6",
+    storageBucket: "voting-3fdb6.appspot.com",
+    messagingSenderId: "297713038678",
+    appId: "1:297713038678:web:a9a6eca64d0ce0043ed2f7"
+  };
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
